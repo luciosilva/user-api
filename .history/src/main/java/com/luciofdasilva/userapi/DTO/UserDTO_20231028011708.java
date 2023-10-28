2cd44bd911dab8058a8643fba51d3d488710a6e8
luciosilva <sandmanlucio@gmail.com>
2023-10-28 01:18:14 -0300

    Create DTO directory

--- .history/src/main/java/com/luciofdasilva/userapi/DTO/UserDTO_20231028011708.java
+++ .history/src/main/java/com/luciofdasilva/userapi/DTO/UserDTO_20231028011708.java
@@ -1,20 +1,11 @@
-package com.luciofdasilva.userapi.models;
+package com.luciofdasilva.userapi.DTO;
 
 import java.util.Date;
 
-import com.luciofdasilva.userapi.DTO.UserDTO;
+import com.luciofdasilva.userapi.models.User;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+public class UserDTO {
 
-@Entity(name="user")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
     private String nome;
     private String cpf;
     private String endereco;
@@ -22,73 +13,55 @@ public class User {
     private String telefone;
     private Date dataCadastro;
 
-    public static User convert(UserDTO userDTO) {
-		User user = new User();
-		user.setNome(userDTO.getNome());
-		user.setEndereco(userDTO.getEndereco());
-		user.setCpf(userDTO.getCpf());
-		user.setEmail(userDTO.getEmail());
-		user.setTelefone(userDTO.getTelefone());
-		user.setDataCadastro(userDTO.getDataCadastro());
-		return user;
+    public static UserDTO convert(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setNome(user.getNome());
+		userDTO.setEndereco(user.getEndereco());
+		userDTO.setCpf(user.getCpf());
+		userDTO.setEmail(user.getEmail());
+		userDTO.setTelefone(user.getTelefone());
+		userDTO.setDataCadastro(user.getDataCadastro());
+		return userDTO;
 	}
 
-
-	public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getCpf() {
         return cpf;
     }
-
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
     public String getEndereco() {
         return endereco;
     }
-
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getTelefone() {
         return telefone;
     }
-
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
     public Date getDataCadastro() {
         return dataCadastro;
     }
-
     public void setDataCadastro(Date dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
+
 
 
 }
